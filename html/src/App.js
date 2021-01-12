76c76326f38f57d1326c518cfa09b281ac53a72d
code=kage <vptl185@gmail.com>
2021-01-12 15:06:04 +0530

    adding row-named input layer

--- html/src/App.js
+++ html/src/App.js
@@ -1,43 +1,25 @@
 import React from 'react';
-import { Network } from './nn';
-import * as Data from './nn/data'
-
+// import * as Data from './nn/data'
 import axios from 'axios';
+
 import "./App.css"
 
+class Network{
+  constructor(
+    name="NetWork"
+  ){
+    console.log(`Network : ${name}`)
+  }
+}
 
 function App() {
 
- 
   React.useEffect(() => {
-
-    let canvas = document.getElementById("graph");
-    let ctx = canvas.getContext('2d');
-
-    
+    let net = new Network();
   }, [])
 
-
   return (
-    <div className="root" id="root" onClick={clearPop}>
-      <div id="pop" className="pop">
-
-      </div>
-      <span role="img" className="btn-download" onClick={download} > ⏬ </span>
-      <div className="dropdown" id="dropdown">
-        <div className="option selected" onClick={dropDown} >
-          {input.selected}
-          <span role="img" id="btn-drop" className="btn-drop" >
-            🔽
-          </span>
-        </div>
-        <div id="items" className="items">
-
-        </div>
-      </div>
-      
-      <canvas id="graph" style={{ background: "white", overflow: "scroll" }} >
-      </canvas>
+    <div className="root" id="root" >
 
     </div>
   );
---
+++ html/src/nn/data/index.js
@@ -0,0 +1,27 @@
+let dense_0 = {
+    network:{
+        input:{
+            values:{
+                keys:[],
+                values:[]
+            },
+            type:'row-column-pair'
+        },
+        'dense_1':{
+            values:{
+                outputs:[],
+                norms:[]
+            },
+            type:'dense',
+            shape:[128,]
+        },
+        'dense_2':{
+            values:{
+                outputs:[],
+                norms:[]
+            },
+            type:'dense',
+            shape:[3,]
+        }
+    }
+}
